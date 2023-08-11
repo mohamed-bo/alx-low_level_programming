66467b70164c32a11ded83af6ebb79d9b50c0ccc
mohamed-bo <medprgrm@gmail.com>
2023-08-11 11:42:26 +0100

    variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  *main - print lowercase a-z
  *Return: Always 0 (Success)
  */
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  *main - print lowercase and uppercase a-zA-Z using putchar
  *Return: Always 0 (Success)
  */
