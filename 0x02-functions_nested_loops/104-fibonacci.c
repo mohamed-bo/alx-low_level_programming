9773669b9389ac8b1fabc58886cccc64ff2e7b55
mohamed-bo <medprgrm@gmail.com>
2023-08-15 18:03:21 +0100

    functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,43 @@
 
 int main(void)
 {
-	int counter = 0;
-	int i = 1;
-	long f = 1;
-	long s = 1;
+	unsigned long f = 1;
+	unsigned long s = 1;
+	unsigned long sum = 0;
+	int counter;
+	
+	long oversum, f_part1, f_part2, s_part1, s_part2, sum_part1, sum_part2;
 
-	while (counter < 99)
+	printf("1");
+
+	for (counter = 2; counter < 92; counter++)
 	{
-		printf("%d, ", i);
+		sum = f + s;
 		f = s;
-		s = i;
-		i = s + f;
-		counter++;
+		s = sum;
+		printf(", %ld", sum);
 	}
+
+	f_part1 = f / 1000000000;
+	f_part2 = f % 1000000000;
+	s_part1 = s / 1000000000;
+	s_part2 = s % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		oversum = (f_part2 + s_part2) / 1000000000;
+		sum_part2 = (f_part2 + s_part2) - (1000000000 * oversum);
+		sum_part1 = (f_part1 + s_part1) + oversum;
+
+		printf(", %ld%ld", sum_part1, sum_part2);
+
+		f_part1 = s_part1;
+		f_part2 = s_part2;
+		s_part1 = sum_part1;
+		s_part2 = sum_part2;
+	}
+
+
 	printf("\n");
 
 	return (0);
