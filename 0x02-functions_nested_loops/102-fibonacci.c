d68c6ac60d1a75b1c3fce0ca072472bfc8637981
mohamed-bo <medprgrm@gmail.com>
2023-08-15 14:07:03 +0100

    functions_nested_loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,18 @@
 
 int main(void)
 {
+	int counter = 0;
 	int i = 1;
 	long f = 1;
 	long s = 1;
 
-	while (i <= 50)
+	while (counter < 50)
 	{
 		printf("%d, ", i);
 		f = s;
 		s = i;
 		i = s + f;
+		counter++;
 	}
 	printf("\n");
 
