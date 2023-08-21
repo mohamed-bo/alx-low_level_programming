988761b75a8ce3b31c0a153ed5d35e36039a2b4a
mohamed-bo <medprgrm@gmail.com>
2023-08-21 13:03:17 +0100

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,13 +17,19 @@ _putchar(s[i]);
 _putchar('\n');
 }
 
+/**
+ *_strlen -  function that returns the length of a string.
+ * @s: string value
+ *Return: returns the length of a string.
+ */
+
 int _strlen(char *s)
 {
-int i;
+	int i;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
 
-return (i);
+	return (i);
 }
