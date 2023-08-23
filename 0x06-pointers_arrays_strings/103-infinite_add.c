d3ce530c43465d5aea396c9295e6fa89fc664226
mohamed-bo <medprgrm@gmail.com>
2023-08-23 18:01:59 +0100

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,77 @@
 #include <stdio.h>
 
 /**
+* max - funtion that return the max value
+*@i:first number
+*@j:second number
+*Return: the max
+*/
+
+int max(int i, int j)
+{
+	if (i > j)
+		return (i);
+	else
+		return (j);
+}
+
+/**
+* fill_empty - function that remove the first empty cells
+*@r:the char table
+*@size:the max size of table
+*@size_r: the number of first empty cells
+*Return: the reformed char table
+*/
+
+char *fill_empty(char *r, int size, int size_r)
+{
+	int i = 0;
+
+	for (; i < size - size_r; i++)
+	{
+		*(r + i) = *(r + i + size_r + 1);
+	}
+	r[i] = '\0';
+	i++;
+	for (; i < size; i++)
+	{
+		*(r + i) = 0;
+	}
+	return (r);
+}
+
+/**
+* goToLast - go to last index of char
+*@c:the char table
+*Return: the adress if last cell
+*/
+
+char *goToLast(char *c)
+{
+	while (*c != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
+* arrayLength - return the length of sting
+*@c:the char table
+*Return: the length of sting
+*/
+
+int arrayLength(char *c)
+{
+	int i = 0;
+
+	while (*c != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
 * infinite_add - C function that adds two numbers
 *@n1:first number
 *@n2:second number
@@ -9,37 +80,14 @@
 *Return: pointer to result
 */
 
-int max(int i,int j)
-{
-if (i > j)
-		return (i);
-	else
-		return (j);
-}
-char *fill_empty(char *r, int size, int size_r)
-{
-	int i = 0;
-for(; i< size - size_r ;i++)
-	{
-		*(r + i)=*(r +i+ size_r+1);
-	}
-	r[i] = '\0';
-	i++;
-	return (r);
-}
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int sum = 0, size = --size_r, i = 0, j = 0, extra = 0;
-	while (*n1 != '\0')
-	{
-		i++;
-		n1++;
-	}
-	while (*n2 != '\0')
-	{
-		j++;
-		n2++;
-	}
+
+	n1 = goToLast(n1);
+	n2 = goToLast(n2);
+	i = arrayLength(n1);
+	j = arrayLength(n2);
 	if (max(i, j) > size_r)
 		return (0);
 	n1--;
@@ -50,25 +98,25 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		*(r + size_r) = (sum % 10) + '0';
 		extra = sum / 10;
 	}
-	for (; *n1; n1-- , size_r--)
+	for (; *n1; n1--, size_r--)
 	{
-		sum = (*n1 - '0') + extra; 
+		sum = (*n1 - '0') + extra;
 		*(r + size_r) = (sum % 10) + '0';
 		extra = sum / 10;
 	}
-	for (; *n2; n2-- ,  size_r--)
+	for (; *n2; n2--, size_r--)
 	{
-		sum = (*n2 - '0') + extra; 
+		sum = (*n2 - '0') + extra;
 		*(r + size_r) = (sum % 10) + '0';
-		extra = sum / 10; 
+		extra = sum / 10;
 	}
-	if (extra !=0)
+	if (extra != 0)
 	{
 		*(r + size_r) = extra + '0';
-        size_r--; 
+		size_r--;
 	}
-	if (size_r<0)
+	if (size_r < 0)
 		return (0);
-	fill_empty(r,size,size_r);
-    return (r);
+	fill_empty(r, size, size_r);
+	return (r);
 }
