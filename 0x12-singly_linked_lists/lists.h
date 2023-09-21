d69f12a8a00787212bce23509968d500e9c28213
mohamed-bo <medprgrm@gmail.com>
2023-09-21 20:54:34 +0100

    singly_linked_lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,7 +10,7 @@
  * @str: string -pointer string
  * @len: length of the string
  * @next: adress of the next node
-
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -24,5 +24,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
 
 #endif
