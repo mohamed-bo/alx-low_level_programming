198975131033dbb5f72228316adf07ba212377bb
mohamed-bo <medprgrm@gmail.com>
2023-09-21 21:04:10 +0100

    singly_linked_lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,6 +2,22 @@
 #include <string.h>
 
 /**
+ * _strlen - length of a string
+ * @s: string
+ * Return: the length of string
+ */
+int _strlen(const char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+
+return (i);
+}
+
+/**
  * add_node_end - adds a new node at the end of a list_t list.
  * @head: adress of the head node
  * @str: string of new node
@@ -28,3 +44,4 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	return (new);
 }
+
