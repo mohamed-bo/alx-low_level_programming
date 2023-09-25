4c4892a2d31d85b91bd369d4705c030e67744f71
mohamed-bo <medprgrm@gmail.com>
2023-09-25 17:00:57 +0100

    betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *toDelete;
 	unsigned int i;
 
-	if (head == NULL || *head == NULL || index < 0)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	if (index == 0)
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	new = malloc(sizeof(listint_t));
-	if (head == NULL || new == NULL || idx < 0)
+	if (head == NULL || new == NULL)
 		return (NULL);
 
 	new->n = n;
