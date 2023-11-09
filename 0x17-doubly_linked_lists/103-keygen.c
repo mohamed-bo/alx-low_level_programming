1460ef17f46606294dd804622e1805ffd5fd7b49
mohamed-bo <medprgrm@gmail.com>
2023-11-09 17:28:55 +0100

    inchalah it works

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - keygen ckrack me 5
+ * @argc: size of argv
+ * @argv: table agrument
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	return 1;
+}
