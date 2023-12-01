d8a64b54ecfd73ef758d8af389888b335defa1d7
mohamed-bo <medprgrm@gmail.com>
2023-12-01 16:47:53 +0100

    inchalah it works

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,22 +6,20 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int i = 1;
+    hash_node_t *node;
+	unsigned long int i = 0;
+	char *sep = "";
 
 	if (!ht || !ht->array)
 		return;
 	putchar('{');
-	if (((ht->array)[0]))
-	{
-		printf("'%s': '%s'", ((ht->array)[0])->key, ((ht->array)[0])->value);
-	}
 	while (i < ht->size)
 	{
 		node = ((ht->array)[i]);
 		while (node)
 		{
-			printf(", '%s': '%s'", node->key, node->value);
+			printf("%s'%s': '%s'", sep, node->key, node->value);
+			sep = ", ";
 			node = node->next;
 		}
 		i++;
